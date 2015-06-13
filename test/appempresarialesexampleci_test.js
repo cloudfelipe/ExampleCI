addfd3a46fc0bcc1d860d129438818bb65f23265
Your Name <you@example.com>
2015-06-13 11:06:35 -0500

    adding project

---
+++ test/appempresarialesexampleci_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var appempresarialesexampleci = require('../');
+var assert = require('should');
+
+describe('appempresarialesexampleci', function () {
+
+  it('should be awesome', function () {
+    appempresarialesexampleci().should.equal('awesome');
+  });
+
+});
